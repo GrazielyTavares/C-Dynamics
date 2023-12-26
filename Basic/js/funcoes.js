c0cde858b560fab6a94d2247d1eb78a2c5fa6787
Graziely Tavares <grazitds2005@gmail.com>
2023-12-25 23:05:19 -0300

    Create funcoes.js

---
+++ Basic/js/funcoes.js
@@ -0,0 +1,17 @@
+function calcul(peso, altura){
+
+}
+
+function clas(imc){
+
+}
+
+//Principal
+(function () {
+    const peso = 75;
+    const altura = 1.75;
+
+    const imc = calcul(peso, altura);
+    console.log(calcul);
+})();
+
